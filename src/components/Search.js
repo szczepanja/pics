231983269317815158e4989fd122fa17f8739163
Szczepanik Anna <szczepanikanja@gmail.com>
2021-11-28 22:35:49 +0100

    add state to input value

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 class Search extends React.Component {
+  state = {
+    term: "",
+  };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
