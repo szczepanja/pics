c8967c44088c8f8bc79ab75034d6f5a77f73e8f2
Anna Szczepanik <a.szczepanik@adform.com>
2021-11-29 11:47:40 +0100

    pass onSearchSubmit function to create API call

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Searchbar from './Searchbar'
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <Searchbar />
-    </div>
-  )
+class App extends React.Component {
+  onSearchSubmit = (term) => {
+    //here we can make a request of images from API
+    console.log(term)
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <Searchbar onSubmit={this.onSearchSubmit} />
+      </div>
+    )
+  }
 }
 
 export default App
