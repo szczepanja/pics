e3fcdc2a8be90493b3de4dfb41f6a4f90f874fee
Anna Szczepanik <a.szczepanik@adform.com>
2021-11-30 10:40:24 +0100

    add axios get API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import axios from 'axios'
 import Searchbar from './Searchbar'
 
 class App extends React.Component {
   onSearchSubmit = (term) => {
-    //here we can make a request of images from API
-    console.log(term)
+    //fetch data
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID bD1jNDFbYPIgU68UNzzFupY6DWshUwsjo9SSpbVvq6k',
+      },
+    })
   }
 
   render() {
