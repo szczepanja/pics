0f2ceb82e69c239777836542cf4c8cf92104a452
Anna Szczepanik <a.szczepanik@adform.com>
2021-12-01 10:52:19 +0100

    add properties to image array

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Searchbar from './Searchbar'
 import unsplash from '../API/Unsplash'
+import Searchbar from './Searchbar'
+import ImageList from './ImageList'
 
 class App extends React.Component {
   state = { images: [] }
@@ -9,14 +10,16 @@ class App extends React.Component {
     const response = await unsplash.get('/search/photos', {
       params: { query: term },
     })
-    this.setState({ images: response.data.results })
+    this.setState({
+      images: response.data.results
+    })
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <Searchbar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     )
   }
